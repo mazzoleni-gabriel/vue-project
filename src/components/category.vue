2f48dcf75ed6afeff71e4e24d84ec1b34adc20bf
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2018-07-31 21:29:41 -0300

    first commit - initiated project

---
+++ src/components/category.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="slider">
+        <h3>{{categoryName}}</h3>      
+        <span v-on:mouseover="scrollLeft()" v-on:mouseout="clearScroll()" class="handle handlePrev active">
+            <i class="fa fa-caret-left" aria-hidden="true"></i>
+        </span>
+
+        <div ref="scroller" class="row">
+            <div class="row__inner">
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+                <movie movieName="Pantera negra" image="https://boygeniusreport.files.wordpress.com/2018/04/black-panther.jpg?quality=98&strip=all&w=1200"></movie>
+            </div>
+        </div>
+
+        <span v-on:mouseover="scrollRight()" v-on:mouseout="clearScroll()" class="handle handleNext active">
+            <i class="fa fa-caret-right" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+
+import movie from './movie.vue';
+
+export default {
+  props: ["categoryName"],
+  data() {
+    return {
+      appName: "Netflix de Pobre",
+      interval: null
+    };
+  },
+  components: {
+      movie
+  },
+  methods: {
+    scrollRight() {
+      this.interval = setInterval(() => {
+        this.$refs.scroller.scrollLeft += 1;
+      }, 5);
+    },
+    scrollLeft() {
+      this.interval = setInterval(() => {
+        this.$refs.scroller.scrollLeft -= 1;
+      }, 5);
+    },
+    clearScroll() {
+      clearInterval(this.interval);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
+
---
+++ src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{categoryName}}</h1>
+</template>
+
+<script>
+export default{  
+  props:['categoryName']
+}
+</script>
+
+<style lang="scss">
+h1{
+  color: red;
+}
+</style>
+
